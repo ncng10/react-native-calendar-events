a0ec49a264caefff9cb24c0ef6dc4ee605e681f7
Nolan Cong <nolan@thisapp.so>
2023-07-26 00:28:41 -0700

    set type to property

--- src/index.d.ts
+++ src/index.d.ts
@@ -72,7 +72,7 @@ interface Attendee {
   /** iOS ONLY - The The phone number of the attendee. */
   phone?: string;
   role: 'Unknown' | 'Required' | 'Optional' | 'Chair' | 'NonParticipant';
-  status: 'Accepted' | 'Completed' | 'Declined' | 'Delegated' | 'InProcess' | 'Pending' | 'Tentative' | 'Unknown' | 0 | 1 | 2 | 3 | 4;
+  status: AttendeeStatus;
   isMe: boolean;
   isOrganizer: boolean;
 }
